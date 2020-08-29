bcc9a9cb552ae1076f46c95ae58d2ced097a9753
connorblackburn98 <42616398+connorblackburn98@users.noreply.github.com>
2020-08-29 15:26:37 +0100

    js directory and variables.js file created
    variables.js file includes basic JavaScript variable notes with examples.

---
+++ js/variables.js
@@ -0,0 +1,30 @@
+// A variable is a named container for some kind of data.
+// We give variable names which are relevant to the data they hold.
+// Good convention suggests always using a letter at the beginning of a variable name and to use camelCase.
+// Variables are great as they allow code to be reused, supporting the DRY theory (Don't Repeat Yourself!).
+
+// The syntax (how to create a variable)
+var X = 'y';
+
+// 'var' is the variable decleration, this is how you tell JacaScript you want to make a variable
+// 'X' here is the name given to the variable. In real life X would be replaced by something more relevant.
+// '=' is the assignment operator, this sets the value of the variable.
+// 'y' is the actual value. Variables can store a wide range of data types. (Mentioned laater in document).
+// ';' is used to complete the variable.
+
+
+// Examples of variables
+var firstName = 'Connor';
+var lastName = 'Blackburn';
+var age = 21;
+var male = true; 
+
+// Results
+// 'console.log();' is used to log the value of the varible to the console.
+console.log(firstName); // Result = Connor
+console.log(lastName); // Result = Blackburn
+console.log(age); // Result = 21
+console.log(male); // Result = true
+
+
+
