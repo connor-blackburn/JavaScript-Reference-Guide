01f8e78654e0c074daad12b2c59a8c0947515e33
connorblackburn98 <connorblackburndev@outlook.com>
2020-09-01 20:09:23 +0100

    Adding & Removing array elements examples added
    Built in JavaScript functions used to add and remove array elements examples added to arrays.js

--- js/arrays.js
+++ js/arrays.js
@@ -1,10 +1,10 @@
 // An array is a list, usually of related elements.
 // As many or as little elements as you please. (max elements = 4.29billion)
-// An array can mix and hold any type of data types.
+// A single array can mix and hold any type of data types.
 // Items in arrays are called elements.
 
 // Array syntax
-// var arrayname = [ 'array element', 'array element2' ];
+// var arrayname = [ 'array element', 'array element2', ];
 
 var names = [
     'Connor',
@@ -13,5 +13,36 @@ var names = [
     'Jeff',
 ];
 
-console.log(names[1]); // Result - Sophie, as array items start at 0.
+console.log(names[1]); // Result - Sophie, as array indexing start at 0.
 console.log(names.length); // Result - 4. Returns the length of the array.
+
+var mixedArray = [
+    'Connor',
+    'Blackburn',
+    21,
+    {male: true},
+    null,
+];
+
+// Adding elements to the end of the array
+names[names.length] = 100;
+console.log(names);
+// Using the Js built in function push to add element to the end of the array
+names.push(102);
+console.log(names);
+
+// Using the Js built in function unshift to add element to the start of the array
+names.unshift('Billy');
+console.log(names);
+
+// Using the Js built in function pop to remove an element off the end of the array
+console.log(names.pop()); // Returns 102 as this is the removed element
+console.log(names);
+
+// Using the Js built in function shift to remove element off the start of the array
+console.log(names.shift()); // Returns Billy as this is the removed element
+console.log(names);
+
+// An easier alternative without built in function to replace an array element.
+names[2] = 'John';
+console.log(names);
