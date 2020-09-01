6a384da07dc02236e66df1f40002d0e049128bc5
connorblackburn98 <connorblackburndev@outlook.com>
2020-09-01 19:36:35 +0100

    Basic array explanation and example added.

---
+++ js/arrays.js
@@ -0,0 +1,17 @@
+// An array is a list, usually of related elements.
+// As many or as little elements as you please. (max elements = 4.29billion)
+// An array can mix and hold any type of data types.
+// Items in arrays are called elements.
+
+// Array syntax
+// var arrayname = [ 'array element', 'array element2' ];
+
+var names = [
+    'Connor',
+    'Sophie',
+    'Bob', 
+    'Jeff',
+];
+
+console.log(names[1]); // Result - Sophie, as array items start at 0.
+console.log(names.length); // Result - 4. Returns the length of the array.
