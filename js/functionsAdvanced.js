2baf83ee8ab2b84fa93714548e2bbdd219f9cd18
connorblackburn98 <connorblackburndev@outlook.com>
2020-09-05 22:19:30 +0100

    Higher order functions examples added

--- js/functionsAdvanced.js
+++ js/functionsAdvanced.js
@@ -11,3 +11,49 @@ function add (a, b) {
 var addExpression = function (a, b) {
     return a + b;
 }
+
+// Higher order functions. 
+// This is a function which takes a function as an argument or returns a function as its return value
+// An array map function is a basic example as it takes the multiply function as an argument
+// Map is a higher order function
+function multiply (num) {
+    return num * 15;
+}
+var numbers = [
+    1,
+    5,
+    7,
+]
+console.log(numbers.map(multiply)); // (3) [15, 75, 105]
+
+function map (list, transform) {
+    var result = [];
+    for (var i = 0; i < list.length; i++) {
+        result.push(transform(list[i], i))
+    } 
+    return result;
+}
+console.log(map(numbers, multiply)); // (3) [15, 75, 105]
+
+// Functions that return a function - this can reduce wasted code and increses reusable code
+function withSalesTax(taxRate) {
+    return function (price) {
+        return Number((price * taxRate).toFixed(2));
+    }
+}
+var standardTaxItems = [
+    100,
+    99.99,
+    82.99,
+]
+var reducedTaxItems = [
+    10,
+    87,
+    12.99,
+]
+
+var standardItemsWithTax = 
+    standardTaxItems.map(withSalesTax(1.2)); //(3) [120, 119.99, 99.59]
+
+var reduceedItemsWithTax = 
+    reducedTaxItems.map(withSalesTax(1.12)); //(3) [11.2, 97.44, 14.55]
