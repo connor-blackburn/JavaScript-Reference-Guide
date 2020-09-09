a66daa54cf27d1e2cfa5b4ae941d7f4566dd0e25
connorblackburn98 <connorblackburndev@outlook.com>
2020-09-09 20:51:16 +0100

    Update scopeAndClosures.js

--- js/scopeAndClosures.js
+++ js/scopeAndClosures.js
@@ -7,7 +7,7 @@ function myFunction () {
 }
 myFunction();
 
-console.log(innerVar); // Uncaught ReferenceError: innerVar is not defined. 
+//console.log(innerVar); // Uncaught ReferenceError: innerVar is not defined. 
 // This is because the var was declared within the function, so is only accessible within the function. It has function level scope.
 
 // Any variable declared outside of a function is known to be within global scope and can be accessed anywhere in the code
@@ -27,5 +27,17 @@ function withSalesTax(taxRate) {
         return Number((price * taxRate).toFixed(2));
     }
  }
+
  var withLowTax = withSalesTax(1.1);
+ console.log(withLowTax(100)); // 110
+
  var withHighTax = withSalesTax(1.2);
+ console.log(withHighTax(100)); // 120
+
+ // Both of these function are actually the inner function of the withSalesTax function with different tax rates which is passed as an argument to the outer function
+ // The value of the argument seems to stay frozen to the value it was when it was called.
+ // Eventhough we pass in the argument 1.2 into the withHighTax(1.2) most recently, the withLowTax(1.1) still remains as 110, the previous call
+ // The key concept to closure is a new scope is created when we call a function, not when we define it.
+ // Once the function is called and the value is returned, the scope is gone.
+
+ // A closure is created when an inner function is returned from an outer function, and the returned function retains access to the outer functions scope even after the outer function has return. At which point, the scope would not usually exist if it was not for the closure
